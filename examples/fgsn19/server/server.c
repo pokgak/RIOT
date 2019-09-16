ac7f40622ac6b3b7a1431556f5db4443f07cc116
Aiman Ismail <muhammadaimanbin.ismail@haw-hamburg.de>
2019-09-16 17:51:03 +0300

    update server to latest dtls sock api

--- examples/fgsn19/server/server.c
+++ examples/fgsn19/server/server.c
@@ -31,21 +31,18 @@ void start_server(void)
     local_ep.port = DTLS_SERVER_PORT;
     sock_udp_create(&udp_sock, &local_ep, NULL, 0);
 
-    sock_dtls_queue_t queue;
-    sock_dtls_session_t queue_array[10];
-    sock_dtls_session_t rcv_session;
+    sock_dtls_session_t rcv_session = {0};
 
-    res = sock_dtls_create(&sock, &udp_sock, 0);
+    res = sock_dtls_create(&sock, &udp_sock, DTLS_SOCK_SERVER_TAG, SOCK_DTLS_1_2, SOCK_DTLS_SERVER);
     if (res < 0) {
         puts("Error creating DTLS sock");
         return;
     }
 
-    credman_tag_t tags[] = { DTLS_SOCK_SERVER_TAG };
 #ifdef DTLS_PSK
     credman_credential_t credential = {
         .type = CREDMAN_TYPE_PSK,
-        .tag = tags[0],
+        .tag = DTLS_SOCK_SERVER_TAG,
         .params = {
             .psk = {
                 .key = { .s = psk_key_0, .len = sizeof(psk_key_0) - 1, },
@@ -61,7 +58,7 @@ void start_server(void)
 #ifdef DTLS_ECC
     credman_credential_t credential = {
         .type = CREDMAN_TYPE_ECDSA,
-        .tag = tags[0],
+        .tag = DTLS_SOCK_SERVER_TAG,
         .params = {
             .ecdsa = {
                 .private_key = ecdsa_priv_key,
@@ -77,9 +74,6 @@ void start_server(void)
         return;
     }
 #endif /* DTLS_ECC */
-
-    sock_dtls_register_credential_tags(&sock, tags,sizeof(tags) / sizeof(tags[0]));
-    sock_dtls_init_server(&sock, &queue, queue_array, 10);
 
     while (1) {
         puts("waiting for packets");
